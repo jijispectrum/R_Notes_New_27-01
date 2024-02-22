3fa35b1fb4df42b526bd48d91d90c24045958f14
jijispectrum <156187339+jijispectrum@users.noreply.github.com>
2024-02-22 09:46:03 +0530

    Add files via upload

--- hist_box_R.R
+++ hist_box_R.R
@@ -31,3 +31,33 @@ ggsave("histogram.png", width = 8, height = 6, dpi = 300)
 
 
 
+# Give the chart file a name.
+png(file = "boxplot_with_notch.png")
+
+# Plot the chart.
+boxplot(mpg ~ cyl, data = mtcars, 
+        xlab = "Number of Cylinders",
+        ylab = "Miles Per Gallon", 
+        main = "Mileage Data",
+        notch = TRUE, 
+        varwidth = TRUE, 
+        col = c("green","yellow","purple"),
+        names = c("High","Medium","Low")
+)
+# Save the file.
+dev.off()
+
+
+
+library(ggplot2)
+
+# Create the boxplot
+ggplot(mtcars, aes(x = factor(cyl, labels = c("High", "Medium", "Low")), y = mpg, fill = factor(cyl))) +
+  geom_boxplot(notch = TRUE, varwidth = TRUE) +
+  scale_fill_manual(values = c("green", "yellow", "purple")) +
+  labs(title = "Mileage Data", x = "Number of Cylinders", y = "Miles Per Gallon") +
+  theme_bw()
+
+# Save the plot as a png file
+ggsave("boxplot_with_notch.png", width = 8, height = 6, dpi = 300)
+
